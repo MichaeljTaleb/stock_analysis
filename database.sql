0584783ce22fbad06cb1dbc71f74245ca46398b7
Michael Taleb <123047866+MichaeljTaleb@users.noreply.github.com>
2024-05-26 11:06:23 +0300

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,19 @@
+/*
+	Creating columns and template
+*/
+CREATE TABLE authors ( 
+    correct_predictions INT,
+    total_predictions INT
+);
+
+-- /*
+-- 	Writing to database
+-- */
+-- INSERT INTO new_data(textvalue)
+-- VALUES('Hello');
+
+/*
+	Reading from database
+*/
+SELECT *
+FROM authors; 
