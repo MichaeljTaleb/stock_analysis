3e53d700e4c15b90b416b562fb0896eafa7beb4e
Michael Taleb <123047866+MichaeljTaleb@users.noreply.github.com>
2024-05-27 13:43:14 +0300

    Update article_data.py

--- APIs/article_data.py
+++ APIs/article_data.py
@@ -2,40 +2,69 @@
 # email: michael.j.taleb@vanderbilt.edu
 # description: API call that returns the article and info about the article
 #              but must use article id found in a different API call
-#			   youtube tutorial: https://www.youtube.com/watch?v=q-Zgekj-ng0
+#	       youtube tutorial: https://www.youtube.com/watch?v=q-Zgekj-ng0
+#	       rapidAPI link: https://rapidapi.com/apidojo/api/seeking-alpha
 
 import requests
 
-# function that gets API call that gets data
-# paramteter id: the id of the article to read gotten from a different API call
-def get_article(id):
-	url = "https://seeking-alpha.p.rapidapi.com/articles/get-details"
 
-	querystring = {"id":id}
+# this is a test funciton to get the id of the lastest articles
+# this should later be updated to get either articles between a time range or on a certain stock
+def get_article_id():
+    url = "https://seeking-alpha.p.rapidapi.com/articles/v2/list"
 
-	headers = {
-		"X-RapidAPI-Key": "8a28ed0502msh5d73d744c8833f2p1bd458jsn6b9c2f7556ab",
-		"X-RapidAPI-Host": "seeking-alpha.p.rapidapi.com"
-	}
+    querystring = {"size": "20", "number": "1", "category": "latest-articles"}
 
+    headers = {
+        "X-RapidAPI-Key": "8a28ed0502msh5d73d744c8833f2p1bd458jsn6b9c2f7556ab",
+        "X-RapidAPI-Host": "seeking-alpha.p.rapidapi.com"
+    }
 
-	response = requests.get(url, headers=headers, params=querystring).json()
+    response = requests.get(url, headers=headers, params=querystring).json()
+    print(response)
 
-# 	data to print/return
-	attributes = response['data']['attributes']
-	date = attributes['publishOn']
-	title = attributes['title']
-	summary = attributes['summary']
-	article = attributes['content']
-	author_id = response['data']['relationships']['author']['data']['id']
-	return date, title, summary, article, author_id
+get_article_id()
 
 
-date, title, summary, article, author_id = get_article()
+#     article_id = response['data'][0]['id']
+#     return article_id
+#
+#
+# article_id = get_article_id()
+# print(article_id)
 
-print(date)
-print(title)
-print(summary)
-print(article)
-print(author_id + "\n")
-# print()
+#
+#
+# # function that gets API call that gets data
+# # paramteter id: the id of the article to read gotten from the different API call above
+# def get_article(id):
+# 	url = "https://seeking-alpha.p.rapidapi.com/articles/get-details"
+#
+# 	querystring = {"id":id}
+#
+# 	headers = {
+# 		"X-RapidAPI-Key": "8a28ed0502msh5d73d744c8833f2p1bd458jsn6b9c2f7556ab",
+# 		"X-RapidAPI-Host": "seeking-alpha.p.rapidapi.com"
+# 	}
+#
+#
+# 	response = requests.get(url, headers=headers, params=querystring).json()
+#
+# # 	data to print/return
+# 	attributes = response['data']['attributes']
+# 	date = attributes['publishOn']
+# 	title = attributes['title']
+# 	summary = attributes['summary']
+# 	article = attributes['content']
+# 	author_id = response['data']['relationships']['author']['data']['id']
+# 	return date, title, summary, article, author_id
+#
+#
+# date, title, summary, article, author_id = get_article()
+#
+# print(date)
+# print(title)
+# print(summary)
+# print(article)
+# print(author_id + "\n")
+# # print()
