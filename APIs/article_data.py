23eefd1d77d78c18c687e6925f6dacde1bbdcc00
MichaeljTaleb <michael.j.taleb@vanderbilt.edu>
2024-05-28 11:37:59 +0300

    Can get single article

--- APIs/article_data.py
+++ APIs/article_data.py
@@ -21,50 +21,40 @@ def get_article_id():
     }
 
     response = requests.get(url, headers=headers, params=querystring).json()
-    print(response)
+    article_id = response['data'][0]['id']
+    return article_id
 
-get_article_id()
+
+# function that gets API call that gets data
+# paramteter id: the id of the article to read gotten from the different API call above
+def get_article(id):
+    url = "https://seeking-alpha.p.rapidapi.com/articles/get-details"
+
+    querystring = {"id": id}
+
+    headers = {
+        "X-RapidAPI-Key": "8a28ed0502msh5d73d744c8833f2p1bd458jsn6b9c2f7556ab",
+        "X-RapidAPI-Host": "seeking-alpha.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring).json()
+
+    # 	data to print/return
+    attributes = response['data']['attributes']
+    date = attributes['publishOn']
+    title = attributes['title']
+    summary = attributes['summary']
+    article = attributes['content']
+    author_id = response['data']['relationships']['author']['data']['id']
+    return date, title, summary, article, author_id
 
 
-#     article_id = response['data'][0]['id']
-#     return article_id
-#
-#
-# article_id = get_article_id()
-# print(article_id)
+article_id = get_article_id()
+date, title, summary, article, author_id = get_article(article_id)
 
-#
-#
-# # function that gets API call that gets data
-# # paramteter id: the id of the article to read gotten from the different API call above
-# def get_article(id):
-# 	url = "https://seeking-alpha.p.rapidapi.com/articles/get-details"
-#
-# 	querystring = {"id":id}
-#
-# 	headers = {
-# 		"X-RapidAPI-Key": "8a28ed0502msh5d73d744c8833f2p1bd458jsn6b9c2f7556ab",
-# 		"X-RapidAPI-Host": "seeking-alpha.p.rapidapi.com"
-# 	}
-#
-#
-# 	response = requests.get(url, headers=headers, params=querystring).json()
-#
-# # 	data to print/return
-# 	attributes = response['data']['attributes']
-# 	date = attributes['publishOn']
-# 	title = attributes['title']
-# 	summary = attributes['summary']
-# 	article = attributes['content']
-# 	author_id = response['data']['relationships']['author']['data']['id']
-# 	return date, title, summary, article, author_id
-#
-#
-# date, title, summary, article, author_id = get_article()
-#
-# print(date)
-# print(title)
-# print(summary)
-# print(article)
-# print(author_id + "\n")
-# # print()
+print("date: " + date + "\n")
+print("title: " + title + "\n")
+print("summary: " + ' '.join(summary) + "\n")
+print("text: " + article + "\n")
+print("author_id: " + author_id + "\n")
+# print()
