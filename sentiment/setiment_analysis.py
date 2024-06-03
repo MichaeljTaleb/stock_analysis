869d3e555545f341c3a81efafb6f01ed0171fdb6
MichaeljTaleb <michael.j.taleb@vanderbilt.edu>
2024-06-03 13:25:44 +0300

    downloading dataset and testing

--- sentiment/setiment_analysis.py
+++ sentiment/setiment_analysis.py
@@ -2,7 +2,7 @@
 # email: michael.j.taleb@vanderbilt.edu
 # Description: this program is a component of the APIs/data_analysis file
 # it is written in a separate program so that it is easier to debug and write
-# it will be pasted into APIs/data_analysis analyse_article funciton after completion and testing
+# it will be pasted into APIs/data_analysis analyse_article function after completion and testing
 # the purpose of this program is to take in an article as a string parameter and return whether or not
 # it indicates to buy or sell a stock
 
@@ -10,27 +10,40 @@
 # active in terminal by "source venv/bin/activate" & "deactivate"
 
 
+# import sys
+# print(sys.executable)
+
+from datasets import load_dataset
+from transformers import BertTokenizer
+
+def tokenize_function(dataset):
+    # return tokenizer(examples['text'], padding="max_length", truncation=True)
+    # max_length is 512, reduced to save time and space.
+    return tokenizer(dataset['text'], padding="max_length", truncation=True, max_length=128)
 
 
-import sys
-print(sys.executable)
-#
-# from datasets import load_dataset
-#
-# dataset = load_dataset("imdb")
-# print(dataset)
-#
-# from transformers import BertTokenizer
-#
-# tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-#
-#
-# def tokenize_function(examples):
-#     return tokenizer(examples['text'], padding="max_length", truncation=True)
-#
-#
-# tokenized_datasets = dataset.map(tokenize_function, batched=True)
-#
+def print_dataset(tokenized_datasets):
+    tokenized_split = tokenized_datasets['train']
+    # Validating dataset
+    print(len(tokenized_split))
+    for i in range(len(tokenized_split)):
+        print(tokenized_split[i])
+        print(i)
+        print()
+
+
+
+
+dataset = load_dataset("csv",
+                       data_files='/Users/michaeltaleb/Documents/archive/all-data.csv',
+                       encoding='ISO-8859-1',
+                       column_names=['label', 'text'])
+dataset_split = dataset['train']
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+
+
+
 # train_testvalid = tokenized_datasets['train'].train_test_split(test_size=0.2)
 # train_dataset = train_testvalid['train']
 # valid_dataset = train_testvalid['test']
